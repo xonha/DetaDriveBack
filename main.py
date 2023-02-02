e433c5475220048c7dcabd4ced39ff4a8f16c67c
henrique <holiveira@wattio.com.br>
2023-02-02 19:04:29 -0300

    fix: removed /hello route

--- main.py
+++ main.py
@@ -36,12 +36,6 @@ app.include_router(
     dependencies=[Depends(auth.auth_middleware)],
 )
 
-# create a Hello World endpoint
-@app.get("/hello")
-async def hello():
-    return {"message": "Hello World"}
-
-
 if __name__ == "__main__":
     import uvicorn
 
