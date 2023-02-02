8060fc7fe8881399c39ba38b881258dedf586269
henrique <holiveira@wattio.com.br>
2023-02-02 18:11:17 -0300

    fix: removed router duplicated prefix

--- source/routers/trash.py
+++ source/routers/trash.py
@@ -5,16 +5,16 @@ from source import deta
 router = APIRouter()
 
 
-@router.get("/trash", tags=["Trash"])
+@router.get("", tags=["Trash"])
 async def get_trash(req: Request):
     return await deta.get_trash(req)
 
 
-@router.patch("/trash/{file_key}", tags=["Trash"])
+@router.patch("/{file_key}", tags=["Trash"])
 async def restore_file(req: Request, file_key: str):
     return await deta.restore_file(req, file_key)
 
 
-@router.delete("/trash/{file_key}", tags=["Trash"])
+@router.delete("/{file_key}", tags=["Trash"])
 async def delete_file(req: Request, file_key: str):
     return await deta.delete_file(req, file_key)
